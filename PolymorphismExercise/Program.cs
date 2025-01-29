91c365aebd24249003dd40b3f5971e6f47f91fbc
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-29 16:56:31 -0500

    complete

--- PolymorphismExercise/Program.cs
+++ PolymorphismExercise/Program.cs
@@ -38,6 +38,23 @@
             emp = new CommissionEmployee(103, "Mark", 10000, 0.15);
             Console.WriteLine(emp);
             Console.WriteLine($"\nEarnings = {emp.Earnings():C}\n\n");
+
+            Console.WriteLine("\n\n\n");
+
+
+
+            // real-life use of polymorphism
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new SalariedEmployee(101, "John", 1000);
+            employees[1] = new HourlyEmployee(102, "Anne", 50, 20);
+            employees[2] = new CommissionEmployee(103, "Mark", 10000, 0.15);
+
+            foreach (Employee em in employees)
+            {
+                Console.WriteLine(em);
+                Console.WriteLine($"\nEarnings = {em.Earnings():C}\n\n");
+            }
         }
     }
 }
