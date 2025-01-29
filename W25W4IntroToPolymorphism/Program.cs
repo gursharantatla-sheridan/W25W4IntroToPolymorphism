ce2ba05562d2ffd4834e890d6a6c6e18bf531b91
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-29 15:23:45 -0500

    Add project files.

---
+++ W25W4IntroToPolymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿    namespace W25W4IntroToPolymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
