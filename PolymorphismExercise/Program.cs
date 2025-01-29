e3ed1fa2b9c5d3b64c11c1122eb6b2ea92d897bd
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-29 16:51:06 -0500

    with poly

--- PolymorphismExercise/Program.cs
+++ PolymorphismExercise/Program.cs
@@ -17,6 +17,27 @@
             CommissionEmployee commEmp = new CommissionEmployee(103, "Mark", 10000, 0.15);
             Console.WriteLine(commEmp);
             Console.WriteLine($"\nEarnings = {commEmp.Earnings():C}\n\n");
+
+
+            Console.WriteLine("\n\n\n");
+
+
+
+            // with polymorphism
+
+            Employee emp;
+
+            emp = new SalariedEmployee(101, "John", 1000);
+            Console.WriteLine(emp);
+            Console.WriteLine($"\nEarnings = {emp.Earnings():C}\n\n");
+
+            emp = new HourlyEmployee(102, "Anne", 50, 20);
+            Console.WriteLine(emp);
+            Console.WriteLine($"\nEarnings = {emp.Earnings():C}\n\n");
+
+            emp = new CommissionEmployee(103, "Mark", 10000, 0.15);
+            Console.WriteLine(emp);
+            Console.WriteLine($"\nEarnings = {emp.Earnings():C}\n\n");
         }
     }
 }
