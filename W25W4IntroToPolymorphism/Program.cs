576ac301b10fe618b4717236689613d84b7602ee
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-29 15:48:19 -0500

    intro to polymorphism

--- W25W4IntroToPolymorphism/Program.cs
+++ W25W4IntroToPolymorphism/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BaseClass objBase = new BaseClass();
+            objBase.Show();
+
+            //DerivedClass objDerived = new DerivedClass();
+            //objDerived.Show();
+
+            objBase = new DerivedClass();
+            objBase.Show();
         }
     }
 }
